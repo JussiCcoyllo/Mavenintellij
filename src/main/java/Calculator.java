28ee7a81993b211ef2222e777c034a94355e7ec3
Jussi Ccoyllo <jussi.ccoyllo@gmail.com>
2023-02-14 15:49:27 +0100

    Update

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+
+
+    public static double sum(double a, double b){
+        return a + b;
+    }
+    public static double subs(double a, double b){
+        return a - b;
+    }
+    public static double mult(double a, double b){
+        return a * b;
+    }
+    public static double divide(double a, double b){
+        return a / b;
+    }
+     public static double power(double a, double b){
+        return Math.pow(a, b);
+    }
+
+}
